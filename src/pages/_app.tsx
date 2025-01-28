6ca0783cdf09644d3f7cd70418ce9db6f331b66e
soomin <s2305@e-mirim.hs.kr>
2025-01-29 00:01:21 +0900

    [2.3] 프로그래매틱한 페이지 이동(함수 or 이벤트 발생 시)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push("/test");
+  };
+
   return (
     <>
       <header>
@@ -11,6 +18,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <Link href={"/"}>search</Link>
         &nbsp;
         <Link href={"/"}>book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
       </header>
       <Component {...pageProps} />
     </>
