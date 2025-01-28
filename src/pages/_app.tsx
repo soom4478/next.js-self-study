f69ad21f0899d76b8a151aaf8060944500311e5e
soomin <s2305@e-mirim.hs.kr>
2025-01-28 23:34:14 +0900

    [2.3] Link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <header>글로벌 헤더</header>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/"}>search</Link>
+        &nbsp;
+        <Link href={"/"}>book/1</Link>
+      </header>
       <Component {...pageProps} />
     </>
   );
