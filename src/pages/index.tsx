bf28ab5192fc4eb993920e4de6253b1428641740
soomin <s2305@e-mirim.hs.kr>
2025-01-29 20:28:40 +0900

    [2.8] 페이지 별 레이아웃 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-// CSS Module
+import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
+import { ReactNode } from "react";
 
 export default function Home() {
   return (
@@ -9,3 +10,8 @@ export default function Home() {
     </>
   );
 }
+
+// 개별 레이아웃 설정
+Home.getLayout = (page: ReactNode)=> {
+  return <SearchableLayout>{page}</SearchableLayout>
+} // 페이지에 레이아웃을 감싼 형태로 리턴턴
