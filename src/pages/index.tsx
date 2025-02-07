76416a5d0a8c233a884668c63fdcafd2929d4038
soomin <s2305@e-mirim.hs.kr>
2025-02-08 00:07:04 +0900

    [2.9] index ui 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Home() {
   return (
-    <>
-      <h1 className={style.h1}>인덱스</h1>
-      <h2 className={style.h2}>H2</h2>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => <BookItem key={book.id} {...book}/>)}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => <BookItem key={book.id} {...book}/>)}
+      </section>
+    </div>
   );
 }
 
 // 개별 레이아웃 설정
 Home.getLayout = (page: ReactNode)=> {
   return <SearchableLayout>{page}</SearchableLayout>
-} // 페이지에 레이아웃을 감싼 형태로 리턴턴
+} // 페이지에 레이아웃을 감싼 형태로 리턴
